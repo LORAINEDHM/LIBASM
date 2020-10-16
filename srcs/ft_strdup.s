de676b1fd381695327d2326361f920c904fd056f
Loraine Duhamel <lduhamel@mi-r6-p1.s19.be>
2020-10-16 17:43:53 +0200

    strdup ok

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -6,13 +6,8 @@
     global	_ft_strdup
 
             	section	.text
-				xor rax, rax
-				xor	rsi, rsi
 
 _ft_strdup:
-				;mov     rax, 0x2000003		; 'name' of write 
-                                            ; syscall table : (https://opensource.apple.com/source/xnu/xnu-1504.3.12/bsd/kern/syscalls.master or http://shell-storm.org/shellcode/files/syscalls.html)
-                                            ; on macos, need to add 0x2000000 to the unix syscall
            		;syscall						; to call write
 				;jc      error				; if meeting an error (), the error value is put into rax, 
 				;							; and we jump to error function (jump carry)
@@ -24,22 +19,17 @@ _ft_strdup:
 				call	_ft_strlen			; to know size to allocate
 				add		rax, 1				; +1 for '\0'
 				mov		rdi, rax			;
+				mov		rdi, 0
 				call    _malloc				; returns a pointer
 				pop 	rdi					; to have the string get back into rdi
+				cmp     rax, 0				; if rax = NULL, it means malloc failed to allocate the block of memory
+				je		end
 				mov		rsi, rdi
 				mov		rdi, rax
 				call	_ft_strcpy			; rax = pointer to string copy located at allocated space address
-
-				;cmp     rax, 0				; if rax = NULL, it means malloc failed to allocate the block of memory
 				ret
 
-error:
-            	;mov     r8, rax				; using temp to store the error value
-            	;call    ___error			; to put the address location of errno into rax value
-            	;mov     [rax], r8			; to put the error value (stored in 58) at this address location
-            	;mov     rax, -1				; rax may return -1
-            	;ret
 
 end:
-				mov	rax, 0
+				mov	rax, -1
 				ret
